6408a477877f6a9e2b113296a0da1e6137bb315c
robertliwpe <robert.li@wpengine.com>
2023-07-24 21:53:51 +1000

    Update for bc

--- 504.sh
+++ 504.sh
@@ -10,7 +10,7 @@ echo ${installaccount};
 
 for i in ${installaccount}; 
 do
-    errorcount=$(zcat -f /var/log/nginx/${i}.access.log /var/log/nginx/${i}.access.log.1 /var/log/nginx/${i}.access.log.2 /var/log/nginx/${i}.access.log.3 /var/log/nginx/${i}.access.log.4 /var/log/nginx/${i}.access.log.5 /var/log/nginx/${i}.access.log.6 | grep '|50[0-9]|' | wc -l);
+    errorcount=$(zcat -f /var/log/nginx/${i}.access.log /var/log/nginx/${i}.access.log.1 /var/log/nginx/${i}.access.log.2 /var/log/nginx/${i}.access.log.3 /var/log/nginx/${i}.access.log.4 /var/log/nginx/${i}.access.log.5 /var/log/nginx/${i}.access.log.6 | grep '|50[0-9]|' | wc -l | bc);
     echo ${errorcount};
     errorsum=$(${errorcount} + ${errorsum} | bc);
     echo ${errorsum};

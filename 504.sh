0de6b8f7eaf47e0e2fc90207da1600dd9ff2e4ce
robertliwpe <robert.li@wpengine.com>
2023-07-24 21:56:37 +1000

    Update for expr

--- 504.sh
+++ 504.sh
@@ -12,7 +12,7 @@ for i in ${installaccount};
 do
     errorcount=$(zcat -f /var/log/nginx/${i}.access.log /var/log/nginx/${i}.access.log.1 /var/log/nginx/${i}.access.log.2 /var/log/nginx/${i}.access.log.3 /var/log/nginx/${i}.access.log.4 /var/log/nginx/${i}.access.log.5 /var/log/nginx/${i}.access.log.6 | grep '|50[0-9]|' | wc -l | bc);
     echo ${errorcount};
-    errorsum=$(${errorcount} + ${errorsum} | bc);
+    errorsum=$(expr "${errorcount} + ${errorsum}" | bc);
     echo ${errorsum};
 done
 

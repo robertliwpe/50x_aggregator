67fd9ae78b5c11032303364b0a52f1bed5512ee0
robertliwpe <robert.li@wpengine.com>
2023-07-24 21:58:33 +1000

    Update for verbosity

--- 504.sh
+++ 504.sh
@@ -6,14 +6,14 @@ read installinit;
 installaccount=$(waldo ${installinit} | tail -2 | head -1 | cut -d':' -f2);
 errorsum=0;
 
-echo ${installaccount};
+echo "Associated Live Installs Found: ${installaccount}";
 
 for i in ${installaccount}; 
 do
     errorcount=$(zcat -f /var/log/nginx/${i}.access.log /var/log/nginx/${i}.access.log.1 /var/log/nginx/${i}.access.log.2 /var/log/nginx/${i}.access.log.3 /var/log/nginx/${i}.access.log.4 /var/log/nginx/${i}.access.log.5 /var/log/nginx/${i}.access.log.6 | grep '|50[0-9]|' | wc -l | bc);
-    echo ${errorcount};
+    # echo ${errorcount};
     errorsum=$(expr "${errorcount} + ${errorsum}" | bc);
-    echo ${errorsum};
+    # echo ${errorsum};
 done
 
-printf "\r\nFor Account of ${installinit} N of 504s for 7 days: ${errorsum}\r\n";
+printf "For Account of ${installinit} N of 504s for 7 days: ${errorsum}\r\n";
